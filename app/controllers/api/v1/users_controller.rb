479c39a23354fbb5b44e7a8be5f351125611fa89
alejoescobar <alejoescobac@gmail.com>
2016-02-20 13:58:42 -0500

    Add create, update and destroy actions for users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,31 @@ class Api::V1::UsersController < ApplicationController
     respond_with User.find(params[:id])
   end
 
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: 201, location: [:api, user]
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  def update
+    user = current_user
+
+    if user.update(user_params)
+      render json: user, status: 200, location: [:api, user]
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  def destroy
+    user = current_user
+    user.destroy
+    head 204
+  end
+
   private
 
     def user_params
