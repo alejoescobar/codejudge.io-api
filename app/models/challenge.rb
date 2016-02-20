0bb00be26521b50a31305df4e3ce0d3bba43bc32
alejoescobar <alejoescobac@gmail.com>
2016-02-20 15:41:13 -0500

    Annotate models

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: challenges
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Challenge < ActiveRecord::Base
   has_many :test_cases
   has_many :submits
--- app/models/submit.rb
+++ app/models/submit.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: submits
+#
+#  id           :integer          not null, primary key
+#  code         :text
+#  status       :integer
+#  challenge_id :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Submit < ActiveRecord::Base
   has_many :code_results
   belongs_to :challenge
