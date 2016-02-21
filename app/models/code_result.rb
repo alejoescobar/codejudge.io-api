920a6843858858cdd95eccb00e065cf55860ce81
alejoescobar <alejoescobac@gmail.com>
2016-02-20 19:22:39 -0500

    add code errors column to code result

--- app/models/code_result.rb
+++ app/models/code_result.rb
@@ -2,15 +2,16 @@
 #
 # Table name: code_results
 #
-#  id              :integer          not null, primary key
-#  actual_output   :text
-#  expected_output :text
-#  passed          :integer
-#  has_errors      :boolean
-#  submit_id       :integer
-#  test_case_id    :integer
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
+#  id               :integer          not null, primary key
+#  actual_output    :text
+#  expected_output  :text
+#  submit_id        :integer
+#  test_case_id     :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  evaluator_status :integer
+#  status           :integer
+#  code_errors      :text
 #
 
 class CodeResult < ActiveRecord::Base
@@ -18,5 +19,5 @@ class CodeResult < ActiveRecord::Base
   belongs_to :test_case
 
   enum evaluator_status: [:ok, :error, :timeout]
-  enum status: [:passed, :error, :timeout]
+  enum status: [:passed, :wrong, :code_timeout]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220234540) do
+ActiveRecord::Schema.define(version: 20160221001542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160220234540) do
     t.datetime "updated_at",       null: false
     t.integer  "evaluator_status"
     t.integer  "status"
+    t.text     "code_errors"
   end
 
   add_index "code_results", ["submit_id"], name: "index_code_results_on_submit_id", using: :btree
