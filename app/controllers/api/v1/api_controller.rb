63754dd6a08d29913330386e622f8d08a28a035e
German Hernandez <muygrafico@gmail.com>
2016-02-21 00:43:48 -0500

    add delete to test_cases

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,3 +1,6 @@
 class Api::V1::ApiController < ApplicationController
   include Authenticable
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || pages_profile_path
+  end
 end
