322f4e712a1ae62a818c5ab0d0a890f3e0afbb9a
alejoescobar <alejoescobac@gmail.com>
2016-02-20 20:48:46 -0500

    Add api as namespace and not constraint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
   # Api definition
-  namespace :api, defaults: { format: :json }, constraints: {subdomain: 'api'}, path: '/' do
+  namespace :api, defaults: { format: :json }, path: '/api' do
     # We are going to list our resources here
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
@@ -16,7 +16,6 @@ Rails.application.routes.draw do
           end
         end
       end
-
     end
   end
   devise_for :users 
